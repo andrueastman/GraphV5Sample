bef39d6caab81c35f5820f000745dba557a3c71e
Eastman <andrueastman@users.noreply.github.com>
2022-12-02 16:32:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Graph;
+using Azure.Identity;
+
+namespace GraphV4Sample
+{
+    class Program
+    {
+        public static async Task Main(string[] _)
+        {
+            // Other TokenCredentials examples are available at https://github.com/microsoftgraph/msgraph-sdk-dotnet/blob/dev/docs/tokencredentials.md
+            string[] scopes = new[] { "User.Read", "User.ReadWrite" };
+            InteractiveBrowserCredentialOptions interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions()
+            {
+                ClientId = "CLIENT_ID"
+            };
+            InteractiveBrowserCredential interactiveBrowserCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+            // GraphServiceClient constructor accepts tokenCredential
+            GraphServiceClient graphServiceClient = new GraphServiceClient(interactiveBrowserCredential, scopes);
+
+            var user = await graphServiceClient.Me.GetAsync();
+
+            Console.WriteLine(user.DisplayName);
+        }
+    }
+}
